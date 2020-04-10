cae4e8910f3671145619da428e78d3f74dbbbd12
John Mazouri <me@jmazouri.com>
2020-04-10 12:17:23 -0400

    Real initial commit
    Doesn't actually run yet but it builds!

---
+++ NovelTD/NovelTD.cpp
@@ -0,0 +1,22 @@
+﻿// NovelTD.cpp : Defines the entry point for the application.
+//
+
+#include "NovelTD.h"
+
+using namespace std;
+
+int main()
+{
+	std::unique_ptr<NovelRT::Graphics::TextRect> textRect;
+
+	auto runner = NovelRT::NovelRunner(0, "NovelTD");
+	auto console = NovelRT::LoggingService(NovelRT::Utilities::Misc::CONSOLE_LOG_APP);
+
+	auto logoTransform = NovelRT::Transform(NovelRT::Maths::GeoVector2<float>(0.0f, 0.0f), 0, NovelRT::Maths::GeoVector2<float>(1.0f, 1.0f));
+
+	//textRect = runner.getRenderer().lock()->createTextRect(rubyGnomerTextTransform, 2, NovelRT::Graphics::RGBAConfig(0, 255, 0, 255), 70, (fontsDirPath / "Gayathri-Regular.ttf").string());
+	//textRect->setText("RubyGnomer");
+
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ NovelTD/NovelTD.h
@@ -0,0 +1,9 @@
+﻿// NovelTD.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include "../lib/NovelRT/include/NovelRT.h"
+
+// TODO: Reference additional headers your program requires here.
